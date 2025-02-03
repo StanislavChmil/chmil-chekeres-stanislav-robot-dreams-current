c214f03991a4a75e5906e78e7d73f6136591e041
StanislavChmil <stas.chmil9@gmail.com>
2025-02-03 14:17:20 +0400

    [MOD] Lesson 2

--- Assets/Lesson 2/Source/List.cs
+++ Assets/Lesson 2/Source/List.cs
@@ -11,13 +11,37 @@ public class List : MonoBehaviour
     [ContextMenu("Print")]
     private void Print()
     {
-        _list.Sort();
-        _list.Add(_value);
-        _list.Remove(_value);
         string msg = "List: ";
         for (int i = 0; i < _list.Count; ++i)
             msg += $"\n{_list[i]}";
         Debug.Log(msg);
+    }
+
+    [ContextMenu("Add")]
+    private void Add()
+    {
+        _list.Add(_value);
+    }
+
+    [ContextMenu("Remove")]
+    private void Remove()
+    {
+        if (_list.Contains(_value))
+        {
+            _list.Remove(_value);
+            Debug.Log($"Removed: {_value}");
+        }
+        else
+        {
+            Debug.Log($"Item '{_value}' not found in the list.");
+        }
+    }
+
+    [ContextMenu("Sort")]
+    private void Sort()
+    {
+        _list.Sort();
+        Debug.Log("List sorted.");
     }
 
     [ContextMenu("Clear List")]
